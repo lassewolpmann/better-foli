822ce1e163d93edf0a60c04c5c2fef2b0fae223a
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-11-21 20:42:24 +0200

    Initial commit with Map showing all Bus stops

---
+++ better-foli/Classes/FoliDataClass.swift
@@ -0,0 +1,80 @@
+//
+//  FoliDataClass.swift
+//  better-foli
+//
+//  Created by Lasse Wolpmann on 21.11.2024.
+//
+
+import Foundation
+
+struct Stop: Decodable {
+    var stop_code: String
+    var stop_name: String
+    var stop_desc: String
+    var stop_lat: Float
+    var stop_lon: Float
+    var zone_id: String
+    var stop_url: String
+    var location_type: Int
+    var parent_station: Int
+    var stop_timezone: String
+    var wheelchair_boarding: Int
+}
+
+struct DecodedArray: Decodable {
+
+    var array: [Stop]
+    
+    // Define DynamicCodingKeys type needed for creating
+    // decoding container from JSONDecoder
+    private struct DynamicCodingKeys: CodingKey {
+
+        // Use for string-keyed dictionary
+        var stringValue: String
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+
+        // Use for integer-keyed dictionary
+        var intValue: Int?
+        init?(intValue: Int) {
+            // We are not using this, thus just return nil
+            return nil
+        }
+    }
+
+    init(from decoder: Decoder) throws {
+
+        // 1
+        // Create a decoding container using DynamicCodingKeys
+        // The container will contain all the JSON first level key
+        let container = try decoder.container(keyedBy: DynamicCodingKeys.self)
+
+        var tempArray = [Stop]()
+
+        // 2
+        // Loop through each key (student ID) in container
+        for key in container.allKeys {
+
+            // Decode Student using key & keep decoded Student object in tempArray
+            let decodedObject = try container.decode(Stop.self, forKey: DynamicCodingKeys(stringValue: key.stringValue)!)
+            tempArray.append(decodedObject)
+        }
+
+        // 3
+        // Finish decoding all Student objects. Thus assign tempArray to array.
+        array = tempArray
+    }
+}
+
+class FoliDataClass {
+    let baseURL = "https://data.foli.fi/gtfs/"
+    
+    func loadStops() async throws -> DecodedArray? {
+        guard let url = URL(string: "\(baseURL)stops") else { return nil }
+        let (data, _) = try await URLSession.shared.data(from: url)
+        let decodedResult = try JSONDecoder().decode(DecodedArray.self, from: data)
+        
+        return decodedResult
+    }
+}
--- better-foli/ContentView.swift
+++ better-foli/ContentView.swift
@@ -6,16 +6,35 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State var stops: [Stop]?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map(bounds: .init(minimumDistance: 10, maximumDistance: 10000)) {
+            if let stops {
+                ForEach(stops, id: \.stop_code) { stop in
+                    let stop_coord = CLLocationCoordinate2D(latitude: CLLocationDegrees(stop.stop_lat), longitude: CLLocationDegrees(stop.stop_lon))
+                    Marker(stop.stop_name, systemImage: "bus", coordinate: stop_coord)
+                        .tint(.orange)
+                }
+            }
         }
-        .padding()
+        .mapControlVisibility(.hidden)
+        .task {
+            let dataClass = FoliDataClass()
+            
+            do {
+                if let data = try await dataClass.loadStops() {
+                    stops = data.array.filter({ stop in
+                        stop.zone_id == "F\u{00d6}LI"
+                    })
+                }
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
