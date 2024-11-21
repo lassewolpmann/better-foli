80ea81547da0fede11b7300e9ae960556141b49d
Lasse Wolpmann <lassewolpmann@outlook.de>
2024-11-21 15:29:12 +0200

    Initial Commit

---
+++ better-foli/better_foliApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_foliApp.swift
+//  better-foli
+//
+//  Created by Lasse Wolpmann on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct better_foliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
